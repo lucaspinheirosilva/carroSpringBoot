d8d2fb83c9152b54690154b877f780c56c980c29
Lucas Pinheiro Silva <30500476+lucaspinheirosilva@users.noreply.github.com>
2021-07-15 19:28:24 -0300

    Delete carros directory

--- carros/src/main/java/com/example/carros/repository/UserRespository.java
+++
@@ -1,8 +0,0 @@
-package com.example.carros.repository;
-
-import com.example.carros.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRespository extends JpaRepository <Usuario,Long>{
-    Usuario findByLogin(String login);
-}
